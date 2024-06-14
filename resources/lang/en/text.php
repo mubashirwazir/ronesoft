098c27c579e4b0cb26be351b000684a42986dfae
Dino <exact@six4.dev>
2024-06-14 23:49:37 +0700

    🌐✨ Add multi-language support for website titles and descriptions
    - Updated language files with translations for title, welcome message, and description in English and Thai.
    - Removed unnecessary HTML and CSS from the welcome page.

--- resources/lang/en/text.php
+++ resources/lang/en/text.php
@@ -1,4 +1,6 @@
 <?php
 return [
-
+    'title' => 'Laravel 11x Web multi-language',
+    'welcome' => 'Welcome to our website',
+    'description' => 'This website supports Thai and English languages',
 ];
