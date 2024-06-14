b02e640f60e8c524580111d03778d0c6ad5eb8c8
Dino <exact@six4.dev>
2024-06-14 23:29:04 +0700

    🚀 Add route for changing language
    Added a new route to change the language using LanguageController.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/change/{lang}',[LanguageController::class,'changeLang'])->name('changeLang');
